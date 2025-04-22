54cb5dd2c2c44a3fa141cada6fa5eeff334e86fb
Fengy2001 <99842837+Fengy2001@users.noreply.github.com>
2025-04-21 18:48:15 -0600

    posts and comments

--- src/reddit/reddit.py
+++ src/reddit/reddit.py
@@ -1,6 +1,8 @@
+import os, time, json, requests, re
+import pandas as pd
+import numpy as np
+
 from reddit_driver import reddit_driver
-import requests
-import json
 
 class reddit:
     def __init__(self, subreddit, account, secrets):
@@ -10,20 +12,33 @@ class reddit:
         self.posts = {}
 
     def get_new_posts(self):
-        subreddit_new_url = self.reddit_url+"/r/"+self.subreddit+"/new?limit=10"
+        subreddit_new_url = self.reddit_url+"/r/"+self.subreddit+"/new"
         response = requests.get(subreddit_new_url, headers=self.driver.get_header()).json()
         for post in response["data"]["children"]:
             if (post["data"]["title"] not in self.posts):
                 self.posts[post["data"]["title"]] = post["data"]["id"]
+        return self.posts
 
-        print(self.posts)
-
-    def get_new_comments(self):
-        subreddit_new_url = self.reddit_url+"/comments/1k0tt5k.json?sort=new"   
-        response = requests.get(subreddit_new_url, headers=self.driver.get_header()).json()
-        for comment in response[1]["data"]["children"]:
-            if "body" in comment["data"]:
-                print(comment["data"]["body"])
-
-test = reddit("wallstreetbets", "default_account", "WSB-data-gathering")
-test.get_new_comments()
+    def save_new_comments(self, thread_id):
+        try:
+            subreddit_new_url = self.reddit_url+f"/comments/{thread_id}.json?sort=new"   
+            response = requests.get(subreddit_new_url, headers=self.driver.get_header()).json()
+            comments = []
+            for comment in response[1]["data"]["children"]:
+                if "body" in comment["data"]:
+                    if "[removed]" in comment["data"]["body"]:
+                        continue
+                    sentence = repr(comment["data"]["body"])
+                    sentence = re.sub(r"^['\"]+|['\"]+$", "", sentence)
+                    sentence = re.sub(r"\\'", "'", sentence)
+                    sentence = re.sub(r'\\"', '"', sentence)
+                    comments.append(f'"{sentence}"')
+            temp_df = pd.DataFrame(comments, columns=["Comments"])
+            temp_df = temp_df.dropna()
+            temp_df["sentiment"] = np.nan
+            if not os.path.exists(self.driver.root+f"\\data\\{time.strftime("%Y-%m-%d")}.csv"):
+                temp_df.to_csv(self.driver.root+f"\\data\\{time.strftime("%Y-%m-%d")}.csv", index=False)
+            else:
+                temp_df.to_csv(self.driver.root+f"\\data\\{time.strftime("%Y-%m-%d")}.csv", index=False, header=False, mode="a")
+        except:
+            raise("Something went terribly wrong.")
